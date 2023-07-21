7f17219b50bf27412eff78c5163ddc5eb8c14317
Chuck Danielsson <chuck.danielsson@weareadaptive.com>
2023-07-20 23:04:00 -0400

    add fdc3 global

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import { createAgent } from '@connectifi/agent-web'
+import { DesktopAgent } from '@finos/fdc3'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -22,3 +24,16 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+const fdc3Global = async () => {
+  //create the agent and assign to the window
+  window.fdc3 = await createAgent(
+    'https://dev.connectifi-interop.com',
+    'local-dotnet@Demo'
+  ) as DesktopAgent;
+
+  //fire the fdc3Ready event
+  document.dispatchEvent(new CustomEvent('fdc3Ready', {}));
+};
+
+fdc3Global()
